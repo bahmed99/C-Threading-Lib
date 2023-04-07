e53f6b91d2896a6e31e266fd9943234fb4a135f0
Simon <villechenaudsimon@gmail.com>
2023-04-07 12:47:32 +0200

    Fix import

--- src/thread.c
+++ src/thread.c
@@ -3,38 +3,39 @@
 #include "queue.h"
 #include <stdlib.h>
 
-
-struct thread_list_elem {
+struct thread_list_elem
+{
     thread_t thread;
-    TAILQ_ENTRY(thread_list_elem) nodes;
+    TAILQ_ENTRY(thread_list_elem)
+    nodes;
 };
 
-TAILQ_HEAD(thread_list_s, thread_list_elem) thread_list;
+TAILQ_HEAD(thread_list_s, thread_list_elem)
+thread_list;
 
-thread_t thread_self() {
-    return TAILQ_FIRST(&thread_list)->thread;
-}
-
-
-void* thread_init() {
+void *thread_init()
+{
     TAILQ_INIT(&thread_list);
 }
 
+thread_t thread_self()
+{
+    return TAILQ_FIRST(&thread_list)->thread;
+}
 
+int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg)
+{
 
-int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
-
-    ucontext_t* current, *main = malloc(sizeof(ucontext_t));
+    ucontext_t *current, *main = malloc(sizeof(ucontext_t));
 
     getcontext(current);
     getcontext(main);
 
     current->uc_link = main;
 
-    makecontext(current, (void(*)(void))func, 1, funcarg);
+    makecontext(current, (void (*)(void))func, 1, funcarg);
 
-
-    struct thread_list_elem* e = malloc(sizeof(struct thread_list_elem));
+    struct thread_list_elem *e = malloc(sizeof(struct thread_list_elem));
     e->thread = *newthread;
     e->thread->uc = current;
 
@@ -43,13 +44,15 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
     return EXIT_SUCCESS;
 }
 
-int thread_yield() {
+int thread_yield()
+{
 
-    if(TAILQ_EMPTY(&thread_list)) {
+    if (TAILQ_EMPTY(&thread_list))
+    {
         return EXIT_SUCCESS;
     }
 
-    struct thread_list_elem* e = TAILQ_FIRST(&thread_list);
+    struct thread_list_elem *e = TAILQ_FIRST(&thread_list);
 
     TAILQ_REMOVE(&thread_list, e, nodes);
     TAILQ_INSERT_TAIL(&thread_list, e, nodes);
@@ -60,3 +63,39 @@ int thread_yield() {
     return EXIT_SUCCESS; // Should not be read
 }
 
+/* attendre la fin d'exécution d'un thread.
+ * la valeur renvoyée par le thread est placée dans *retval.
+ * si retval est NULL, la valeur de retour est ignorée.
+ */
+int thread_join(thread_t thread, void **retval)
+{
+    return EXIT_SUCCESS;
+}
+
+/* terminer le thread courant en renvoyant la valeur de retour retval.
+ * cette fonction ne retourne jamais.
+ *
+ * L'attribut noreturn aide le compilateur à optimiser le code de
+ * l'application (élimination de code mort). Attention à ne pas mettre
+ * cet attribut dans votre interface tant que votre thread_exit()
+ * n'est pas correctement implémenté (il ne doit jamais retourner).
+ */
+void thread_exit(void *retval)
+{
+}
+
+int thread_mutex_init(thread_mutex_t *mutex)
+{
+}
+
+int thread_mutex_destroy(thread_mutex_t *mutex)
+{
+}
+
+int thread_mutex_lock(thread_mutex_t *mutex)
+{
+}
+
+int thread_mutex_unlock(thread_mutex_t *mutex)
+{
+}
