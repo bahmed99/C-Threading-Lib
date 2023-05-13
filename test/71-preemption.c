62bbe01601ee0b32ad651996ad24e65362dc78e8
Simon <villechenaudsimon@gmail.com>
2023-05-13 14:27:14 +0200

    New 71 81 tests

--- test/71-preemption.c
+++ test/71-preemption.c
@@ -1,6 +1,7 @@
-#include <assert.h>
+	#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/time.h>
 #include <inttypes.h>
 #include <sys/time.h>
@@ -18,17 +19,28 @@
  * - retour sans thread_exit()
  * - thread_join() sans récupération de la valeur de retour
  */
+#define NB_ITER     100
+#define ITER_LENGTH 1000000
 
+static int    fini  = 0;
+static double score = 0;
+static long  *values = NULL;
 
 static void * thfunc( void *arg )
 {
     unsigned long i, j = 0;
+    int me = (intptr_t)arg;
 
-    for(i=0; i<100;i++) {
-	for(j=0; j<10000000;j++) {
+    for(i=0; i<NB_ITER;i++) {
+	for(j=0; j<ITER_LENGTH;j++) {
+	    if (fini) {
+		return NULL;
+	    }
+	    values[me]++;
 	}
 	fprintf(stderr, "%ld ", (intptr_t)arg );
     }
+    fini = 1;
 
     return NULL;
 }
@@ -48,7 +60,13 @@ int main(int argc, char *argv[])
     nb = atoi(argv[1]);
     th = malloc(nb * sizeof(*th));
     if (!th) {
-        perror("malloc");
+        perror("malloc(th)");
+        return -1;
+    }
+
+    values = calloc( nb+1, sizeof(long) );
+    if (!values) {
+        perror("malloc(values)");
         return -1;
     }
 
@@ -60,18 +78,33 @@ int main(int argc, char *argv[])
     }
 
     /* On participe au réchauffement climatique */
-    thfunc( (void*)((intptr_t)-1) );
+    thfunc( (void*)((intptr_t)nb) );
 
     /* on les join tous, maintenant qu'ils sont tous morts */
+    score = values[nb];
     for(i=0; i<nb; i++) {
         err = thread_join(th[i], NULL);
         assert(!err);
-    }
-    gettimeofday(&tv2, NULL);
 
+	score += values[i];
+    }
+
+    gettimeofday(&tv2, NULL);
     us = (tv2.tv_sec-tv1.tv_sec)*1000000+(tv2.tv_usec-tv1.tv_usec);
+
+    score = score / ( (double)(nb+1) * NB_ITER * ITER_LENGTH );
+    printf("\nscore: %lf\n", score );
+
     printf("Programme exécuté en %ld us\n", us);
 
     free(th);
-    return 0;
+    free(values);
+
+    if ( score < .5 ) {
+	return EXIT_FAILURE;
+    }
+    else {
+	printf("Temps attendu pour le programme complet: %e us\n", us / score );
+	return EXIT_SUCCESS;
+    }
 }
--- test/81-deadlock.c
+++ test/81-deadlock.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
 #include "thread.h"
 
 /* test de detection d'un deadlock lors d'un cycle de thread qui joignent tous le suivant.
@@ -13,39 +15,43 @@ int totalerr = 0;
 
 static void * thfunc2(void *dummy __attribute__((unused)))
 {
-  void *res;
-  int err = thread_join(th0, &res);
-  printf("join th2->th0 = %d\n", err);
-  totalerr += err;
-  thread_exit(NULL);
+    void *res;
+    int err = thread_join(th0, &res);
+    printf("join th2->th0 = %d\n", err);
+    totalerr += err;
+    thread_exit(NULL);
 }
 
 static void * thfunc1(void *dummy __attribute__((unused)))
 {
-  void *res;
-  int err = thread_create(&th2, thfunc2, NULL);
-  assert(!err);
-  err = thread_join(th2, &res);
-  printf("join th1->th2 = %d\n", err);
-  totalerr += err;
-  thread_exit(NULL);
+    void *res;
+    int err = thread_create(&th2, thfunc2, NULL);
+    assert(!err);
+    err = thread_join(th2, &res);
+    printf("join th1->th2 = %d\n", err);
+    totalerr += err;
+    thread_exit(NULL);
 }
 
 int main()
 {
-  void *res;
-  int err;
+    void *res;
+    int err;
 
-  th0 = thread_self();
+    th0 = thread_self();
 
-  err = thread_create(&th1, thfunc1, NULL);
-  assert(!err);
-  err = thread_join(th1, &res);
-  printf("join th0->th1 = %d\n", err);
-  totalerr += err;
-  printf("somme des valeurs de retour = %d\n", totalerr);
-  assert(totalerr == -1);
-  thread_exit(NULL);
-  return 0;
+    err = thread_create(&th1, thfunc1, NULL);
+    assert(!err);
+    err = thread_join(th1, &res);
+    printf("join th0->th1 = %d\n", err);
+    totalerr += err;
+    printf("somme des valeurs de retour = %d\n", totalerr);
+    assert(totalerr == EDEADLK);
+
+    if ( totalerr == EDEADLK ) {
+	return EXIT_SUCCESS;
+    }
+    else {
+	return EXIT_FAILURE;
+    }
 }
-
